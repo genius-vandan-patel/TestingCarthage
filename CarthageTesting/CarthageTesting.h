c37eafbe59dca9fdb9de24a626ad35ac5f83d216
Vandan Patel <VandanPatel@Mains-MacBook-Pro.local>
2017-11-07 22:31:31 -0500

    Initial Commit

---
+++ CarthageTesting/CarthageTesting.h
@@ -0,0 +1,19 @@
+//
+//  CarthageTesting.h
+//  CarthageTesting
+//
+//  Created by Vandan Patel on 11/7/17.
+//  Copyright © 2017 Vandan Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageTesting.
+FOUNDATION_EXPORT double CarthageTestingVersionNumber;
+
+//! Project version string for CarthageTesting.
+FOUNDATION_EXPORT const unsigned char CarthageTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageTesting/PublicHeader.h>
+
+
---
+++ TestingCartage/Testing.swift
@@ -0,0 +1,15 @@
+//
+//  Testing.swift
+//  TestingCartage
+//
+//  Created by Vandan Patel on 11/7/17.
+//  Copyright © 2017 Vandan Patel. All rights reserved.
+//
+
+import Foundation
+
+public class Testing {
+    public func printSomething() {
+        print("Print Something")
+    }
+}
